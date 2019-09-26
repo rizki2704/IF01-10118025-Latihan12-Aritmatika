aaebff5bbe1ac38094f95988af0a314d220a7f06
rizki2704 <55817452+rizki2704@users.noreply.github.com>
2019-09-26 08:11:11 +0700

    Add files via upload

---
+++ src/if01/pkg10118025/latihan12/aritmatika/IF0110118025Latihan12Aritmatika.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118025.latihan12.aritmatika;
+
+/**
+ *
+ * @author
+ * Nama     : Rizki Restu Illahi
+ * Kelas    : IF01
+ * NIM      : 10118025
+ */
+public class IF0110118025Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
